a00ba4f5e12e561ca42ad3869db253c935ccbc5b
Shelk <welkopr9d@gmail.com>
2018-05-19 09:29:27 +0300

    vk api

---
+++ api/vk.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import vk_api
+
+session = vk_api.VkApi(login='LOGIN',token='TOKEN')
+session.auth()
+
+vk = session.get_api()
+
+friends=vk.friends.get()
+ids = friends['items']
+
+tmp = [id for id in ids]
+x = []
+y = []
+z = {} # просто для проверки
+for id in tmp:
+    user = vk.users.get(user_ids=id)[0]
+    if 'deactivated' not in user:
+        user_name = user['first_name'] + ' ' + user['last_name']
+        user_friends_count = vk.friends.get(user_id=id,fields='name,lists')['count']
+        x.append(user_name)
+        y.append(user_friends_count)
+        z[user_name] = user_friends_count
+
+print(z) # для проверки
+
+x_axe = [c for c in range(0, len(x))]
+
+plt.xticks(x_axe, x, rotation=45, fontsize=7)
+
+plt.scatter(x_axe,y)
+plt.show()
