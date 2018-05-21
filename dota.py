5e08739521539fea8b7bfbb374313563776f6269
Shelkopryad <welkopr9d@gmail.com>
2018-05-21 15:39:42 +0300

    o_O Dota API

--- dota.py
+++ dota.py
@@ -1,4 +1,5 @@
 import dota2api
+import requests
 
 api = dota2api.Initialise('TOKEN')
 match = api.get_match_details(match_id=1000193456)
@@ -6,5 +7,15 @@ match = api.get_match_details(match_id=1000193456)
 
 hist = api.get_heroes()
 list_of_maps = hist['heroes']
-maps = list_of_maps[23]
-print(maps)
+heroes_info = list_of_maps[23]
+for x in list_of_maps:
+    name = x['localized_name']
+    url = x['url_large_portrait']
+    resp = requests.get(url)
+    b = resp.content
+
+    file_name = 'portraits/portrait_' + name + '.png'
+    fobj = open(file_name, "wb") 
+    fobj.write(b) 
+    fobj.close() 
+
